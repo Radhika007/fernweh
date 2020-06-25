40dcd7bdd1b02ea2cbbb03e6b99f1f151f0dc823
Radhika007 <radhika.80599@gmail.com>
2020-06-25 11:46:18 +0530

    Add files via upload

---
+++ Landmark.swift
@@ -0,0 +1,42 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The model for an individual landmark.
+*/
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var category: Category
+    var place: String
+    var info: String
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case cities = "Cities"
+        case islands = "Islands"
+        case ports = "Port Cities"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
---
+++ LandmarkDetail.swift
@@ -0,0 +1,44 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate)
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+
+            CircleImage(image: landmark.image)
+                .offset(x: 0, y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                Text(landmark.place)
+                    .font(.subheadline)
+                Text(landmark.info)
+                    .font(.subheadline)
+                
+            }
+            .padding()
+
+            Spacer()
+        }
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarkData[0])
+    }
+}
