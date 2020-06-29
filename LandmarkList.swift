873002d9341707d2830c93123509c664ee3ec352
Radhika007 <radhika.80599@gmail.com>
2020-06-29 12:59:53 +0530

    Adding Favorites

--- LandmarkList.swift
+++ LandmarkList.swift
@@ -8,23 +8,39 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+    @EnvironmentObject private var userData: UserData
+    
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $userData.showFavoritesOnly) {
+                    Text("Show Favorites Only")
+                }
+                
+                ForEach(userData.landmarks) { landmark in
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                        NavigationLink(
+                            destination: LandmarkDetail(landmark: landmark)
+                                .environmentObject(self.userData)
+                        ) {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
                 }
             }
-        .navigationBarTitle("Choose your place:")
+            .navigationBarTitle(Text("Landmarks"))
         }
     }
 }
 
-struct LandmarkList_Previews: PreviewProvider {
+struct LandmarksList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone 11 Pro","iPad Pro"], id: \.self) { deviceName in
+        ForEach(["iPhone 11 Pro"], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
         }
+        .environmentObject(UserData())
     }
 }
+
