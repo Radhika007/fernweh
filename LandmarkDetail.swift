f368eebdcab45a441f2a6910e560a64c2621feea
Radhika007 <radhika.80599@gmail.com>
2020-06-29 13:01:13 +0530

    Adding a "favorite"

--- LandmarkDetail.swift
+++ LandmarkDetail.swift
@@ -8,37 +8,62 @@ A view showing the details for a landmark.
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var userData: UserData
     var landmark: Landmark
-
+    
+    var landmarkIndex: Int {
+        userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    
     var body: some View {
         VStack {
             MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
-
+            
             CircleImage(image: landmark.image)
                 .offset(x: 0, y: -130)
-                .padding(.bottom, -130)
-
+                .padding(.bottom, -150)
+            
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-                Text(landmark.place)
-                    .font(.subheadline)
-                Text(landmark.info)
-                    .font(.subheadline)
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite.toggle()
+                    }) {
+                        if self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                }
                 
+                HStack(alignment: .top) {
+                    Text(landmark.place)
+                        .font(.subheadline)
+                    Spacer()
+                    Text(landmark.info)
+                        .font(.subheadline)
+                }
             }
             .padding()
-
+            
             Spacer()
         }
-        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarkData[0])
+        let userData = UserData()
+        return LandmarkDetail(landmark: userData.landmarks[0])
+            .environmentObject(userData)
     }
 }
